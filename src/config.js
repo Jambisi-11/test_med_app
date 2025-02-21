9b43133894c497fdb96aeb0f76d1996db84793e7
jambisi-11 <jambis.banji@yahoo.com>
2025-02-21 08:43:53 -0500

    pudated

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const API_URL = window.location.hostname === "localhost" ? "https://labs.cognitiveclass.ai/v2/tools/cloud-ide-kubernetes?ulid=ulid-d8727ddf2c3c48c0753f7d53ca44df7f88cff4e1" : "https://labs.cognitiveclass.ai/v2/tools/cloud-ide-kubernetes?ulid=ulid-d8727ddf2c3c48c0753f7d53ca44df7f88cff4e1";
+console.log(
+    "API_URL :",
+    API_URL
+);
